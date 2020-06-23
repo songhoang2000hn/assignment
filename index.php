69c1f2115d7452b7d4a6769f7d1dc9d413350cd6
songhoang2000hn <66767531+songhoang2000hn@users.noreply.github.com>
2020-06-23 15:43:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
             <!-- <img class="profile-img-card" src="//lh3.googleusercontent.com/-6V8xOA6M7BA/AAAAAAAAAAI/AAAAAAAAAAA/rzlHcD0KYwo/photo.jpg?sz=120" alt="" /> -->
             <img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
             <p id="profile-name" class="profile-name-card"></p>
-            <form class="form-signin">
+            <form class="form-signin" action="Connect.php" method="POST" accept-charset="utf-8">
                 <span id="reauth-email" class="reauth-email"></span>
                 <input type="text" id="inputEmail" class="form-control" placeholder="Email address" required autofocus
                     name="username">
